46b65bb6e772f8cb779c9ff9cf75e2c6f29c9a7b
PARUSSKI <super.pegase@yandex.ru>
2022-11-14 19:16:27 +0500

    first commit

---
+++ app/src/main/java/com/example/crimeintent/Crime.kt
@@ -0,0 +1,22 @@
+package com.example.crimeintent
+
+import java.util.*
+
+data class Crime (var id: UUID = UUID.randomUUID())  {
+    var title:String = ""
+    var date: Date? = null
+    var isSolved: Boolean? = null
+    var _requiresPolice:Boolean = false
+    constructor(id: UUID, title: String, date: Date, isSolved:Boolean,requiresPolice:Boolean):this(id){
+        this.title = title
+        this.date = date
+        this.id = id
+        this.isSolved=isSolved
+        this._requiresPolice = requiresPolice
+    }
+
+
+    fun requiresPolice(): Boolean {
+        return _requiresPolice
+    }
+}
